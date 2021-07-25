0a7c8d6b942e2151c8caf3987f678b5621403d52
charleshpalmer <73086397+charleshpalmer@users.noreply.github.com>
2021-07-25 15:13:52 +0100

    feat: updating logs using redux state

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG } from './types';
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG, SET_CURRENT, CLEAR_CURRENT, UPDATE_LOG } from './types';
 
 //Fetch Logs
 export const getLogs = () => async dispatch => {
@@ -103,11 +103,76 @@ export const deleteLog = (id) => async dispatch => {
 
 };
 
+//Update Log From Server
+export const updateLog = log => async dispatch => {
+
+    try {
+
+        setLoading();
+
+        const res = await fetch(`/logs/${log.id}`, {
+
+            method: 'PUT',
+            body: JSON.stringify(log),
+            headers: {
+
+                'Content-Type': 'application/json'
+
+            }
+
+        });
+
+        const data = await res.json();
+
+        dispatch({
+
+            type: UPDATE_LOG,
+            payload: data
+
+        });
+        
+    } catch (err) {
+
+        dispatch({
+
+            type: LOGS_ERROR,
+            payload: err.response.data
+
+        });
+        
+    };
+
+};
+
+//Set Current Log
+export const setCurrent = log => {
+
+    return{
+
+        type: SET_CURRENT,
+        payload: log
+
+    };
+
+};
+
+//Clear Current Log
+export const clearCurrent = () => {
+
+    return{
+
+        type: CLEAR_CURRENT
+
+    };
+
+};
+
+//Set Loading To True 
 export const setLoading = () => {
 
     return {
 
-        tyle: SET_LOADING
+        type: SET_LOADING
 
     };
 
--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { deleteLog } from '../../actions/logActions';
+import { deleteLog, setCurrent } from '../../actions/logActions';
 import M from 'materialize-css/dist/js/materialize.min.js'
 
-const LogItem = ({ log, deleteLog }) => {
+const LogItem = ({ log, deleteLog, setCurrent }) => {
 
     const onDelete = () => {
 
@@ -21,7 +21,7 @@ const LogItem = ({ log, deleteLog }) => {
 
             <div>
 
-                <a href='#edit-log-modal' className={`modal-trigger ${log.attention ? 'red-text' : 'blue-text'}`}> {log.message} </a>
+                <a href='#edit-log-modal' className={`modal-trigger ${log.attention ? 'red-text' : 'blue-text'}`} onClick={() => setCurrent(log)}> {log.message} </a>
 
                 <br/>
 
@@ -51,6 +51,8 @@ LogItem.propTypes = {
 
     deleteLog: PropTypes.func.isRequired,
 
+    setCurrent: PropTypes.func.isRequired,
+
 }
 
-export default connect(null, { deleteLog })(LogItem)
+export default connect(null, { deleteLog, setCurrent })(LogItem)
